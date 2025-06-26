088eb840bcd24c0ce7b8fc60c8220721036909bf
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 08:45:50 -0400

    Add primitive array model

---
+++ Sources/ATLexiconTools/Models/Lexicons/Helpers/ATPrimitiveArray.swift
@@ -0,0 +1,36 @@
+//
+//  ATPrimitiveArray.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-06-26.
+//
+
+import Foundation
+
+/// An `array` type that contain item primitives.
+public struct ATPrimitiveArray: Codable {
+
+    /// The type value of the object.
+    ///
+    /// This will always be `array`.
+    public var type: String { "array" }
+
+    /// A short description of the object. Optional.
+    public let description: String?
+
+    /// A container of an array of properties.
+    public let items: ATLexiconPrimitive?
+
+    /// The minimum length a `string` object can have. Optional.
+    public let minimumLength: Int?
+
+    /// The maximum length a `string` object can have. Optional.
+    public let maximumLength: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case description
+        case items
+        case minimumLength = "minSize"
+        case maximumLength = "maxSize"
+    }
+}
