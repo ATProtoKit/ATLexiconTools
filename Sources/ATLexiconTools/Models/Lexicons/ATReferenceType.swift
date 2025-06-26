989a1d7c80f119e801b030437dbeae0a8953374e
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 07:24:23 -0400

    Add Ref lexicon model

---
+++ Sources/ATLexiconTools/Models/Lexicons/ATReferenceType.swift
@@ -0,0 +1,31 @@
+//
+//  ATReferenceType.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-06-26.
+//
+
+import Foundation
+
+/// A `ref` type.
+///
+/// In Swift, this would typicaly be the equivalent of assigning a property or `case` to a
+/// `struct`, `class`, or `enum`.
+public struct ATReferenceType: Codable {
+
+    /// The type value of the object.
+    ///
+    /// This will always be `ref`.
+    public var type: String { "ref" }
+
+    /// A short description of the object. Optional.
+    public let description: String?
+
+    /// A reference to another part of a lexicon.
+    public let reference: String
+
+    enum CodingKeys: String, CodingKey {
+        case description
+        case reference = "ref"
+    }
+}
