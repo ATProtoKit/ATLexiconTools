176eee7630e1f4c868ac90b969f57890f5bc70c4
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 06:49:15 -0400

    Add Integer lexicon model

---
+++ Sources/ATLexiconTools/Models/Lexicons/ATIntegerType.swift
@@ -0,0 +1,46 @@
+//
+//  ATIntegerType.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-06-26.
+//
+
+import Foundation
+
+/// A `integer` type.
+///
+/// In Swift, this would be the equivalent to the `Int` type.
+public struct ATIntegerType: Codable {
+
+    /// The type value of the object.
+    ///
+    /// This will always be `integer`.
+    public var type: String { "integer" }
+
+    /// A short description of the object. Optional.
+    public let description: String?
+
+    /// The minimum number. Optional.
+    public let minimum: Int?
+
+    /// The maximum number. Optional.
+    public let maximum: Int?
+
+    /// A closed set of allowed values. Optional.
+    public let enumValue: [Int]?
+
+    /// A default value for the field. Optional.
+    public let defaultValue: Int?
+
+    /// A fixed value for the field. Optional.
+    public let constant: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case description
+        case minimum
+        case maximum
+        case enumValue = "enum"
+        case defaultValue = "default"
+        case constant = "const"
+    }
+}
