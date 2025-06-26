d238015d7b8c1754229c5d746c61bda89793d43b
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 10:19:14 -0400

    Add primary lexicon model for Procedure

---
+++ Sources/ATLexiconTools/Models/Lexicons/Primary/ProcedureDefinition.swift
@@ -0,0 +1,32 @@
+//
+//  ProcedureDefinition.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-06-26.
+//
+
+import Foundation
+
+/// A primary `procedure` type definition.
+public struct ProcedureDefinition: Codable {
+
+    /// The type value of the object.
+    ///
+    /// This will always be `procedure`.
+    public var type: String = "procedure"
+
+    /// A short description explaining the definition. Optional.
+    public let description: String?
+
+    /// A dictionary of query parameters for the HTTP request. Optional.
+    public let parameters: [String: ATParamsType]?
+
+    /// The body that is returned from the server as a result of the request. Optional.
+    public let output: LexiconHTTPBody?
+
+    /// The request body that is sent to the server. Optional.
+    public let input: LexiconHTTPBody?
+
+    /// An array of errors that might be returned. Optional.
+    public let errors: [ATErrorsType]?
+}
