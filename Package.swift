832b02818a430bc66fb5082bb7367a6ed2ab1096
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 04:41:35 -0400

    Update Package.swift
    - Added platforms.
    - Added dependencies.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,33 @@ import PackageDescription
 
 let package = Package(
     name: "ATLexiconTools",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v13),
+        .tvOS(.v14),
+        .visionOS(.v1),
+        .watchOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ATLexiconTools",
             targets: ["ATLexiconTools"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ATProtoKit/ATSyntaxTools.git", from: "0.1.0"),
+        .package(url: "https://github.com/ATProtoKit/ATCommonTools.git", from: "0.0.10")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ATLexiconTools"),
+            name: "ATLexiconTools",
+            dependencies: [
+                .product(name: "ATSyntaxTools", package: "atsyntaxtools"),
+                .product(name: "ATCommonWeb", package: "atcommontools")
+            ]
+        ),
         .testTarget(
             name: "ATLexiconToolsTests",
             dependencies: ["ATLexiconTools"]
