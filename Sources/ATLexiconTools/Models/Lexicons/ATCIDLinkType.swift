7a3838094ac74462e932ca92cf9a8d219248d5d9
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 07:20:00 -0400

    Add CID-Link lexicon type

---
+++ Sources/ATLexiconTools/Models/Lexicons/ATCIDLinkType.swift
@@ -0,0 +1,20 @@
+//
+//  ATCIDLinkType.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-06-26.
+//
+
+import Foundation
+
+/// A `cid-link` type.
+public struct ATCIDLinkType: Codable {
+
+    /// The type value of the object.
+    ///
+    /// This will always be `cid-link`.
+    public var type: String { "cid-link" }
+
+    /// A short description of the object. Optional.
+    public let description: String?
+}
