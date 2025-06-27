6125ff99b30b6e3d4c1c7d91322921dc25e074d4
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 23:20:16 -0400

    Add key decoding strategy to lexicon parsing methods

--- Sources/ATLexiconTools/Models/Lexicons/Helpers/LexiconParser.swift
+++ Sources/ATLexiconTools/Models/Lexicons/Helpers/LexiconParser.swift
@@ -12,17 +12,21 @@ public enum LexiconParser {
 
     /// Parses a single lexicon JSON.
     ///
-    /// - Parameter lexicon: A lexicon JSON.
+    /// - Parameters:
+    ///   - lexicon: A lexicon JSON.
+    ///   - keyDecodingStrategy: A value that determines how to decode a lexicon's coding keys from
+    ///   JSON keys. Defaults to `.useDefaultKeys`.
     /// - Returns: An array of `Lexicon` models.
     ///
     /// - Throws: An error if the decoding fails, such as if the Namespaced Identifier (NSID) is invalid,
     /// the JSON is not structured correctly, or the lexicon is somehow violating any lexicon requirements.
-    public static func parseLexicon(_ lexicon: String) throws -> Lexicon {
+    public static func parseLexicon(_ lexicon: String, keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) throws -> Lexicon {
         guard let lexiconData = lexicon.data(using: .utf8) else {
             throw LexiconToolsError.invalidLexicon
         }
 
         let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = keyDecodingStrategy
         let lexicon = try decoder.decode(Lexicon.self, from: lexiconData)
 
         return lexicon
@@ -30,22 +34,30 @@ public enum LexiconParser {
 
     /// Parses multiple lexicon JSONs.
     ///
-    /// - Parameter lexicons: An array of lexicon JSONs.
+    /// - Parameters:
+    ///   - lexicons: An array of lexicon JSONs.
+    ///   - keyDecodingStrategy: A value that determines how to decode a lexicon's coding keys from
+    ///   JSON keys. Defaults to `.useDefaultKeys`.
     /// - Returns: An array of `Lexicon` models.
     ///
     /// - Throws: An error if the decoding fails, such as if the Namespaced Identifier (NSID) is invalid,
     /// the JSON is not structured correctly, or the lexicon is somehow violating any lexicon requirements.
-    public static func parseMultipleLexicons(_ lexicons: [String]) throws -> [Lexicon] {
+    public static func parseMultipleLexicons(
+        _ lexicons: [String],
+        keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys
+    ) throws -> [Lexicon] {
         var lexiconResults: [Lexicon] = []
 
         for lexicon in lexicons {
-            lexiconResults.append(try Self.parseLexicon(lexicon))
+            lexiconResults.append(try Self.parseLexicon(lexicon, keyDecodingStrategy: keyDecodingStrategy))
         }
 
         return lexiconResults
     }
 
     /// Determines whether the lexicon JSON is valid.
+    ///
+    /// This method assumes that the default keys are being used to decode the JSON keys.
     ///
     /// - Parameter lexicon: The lexicon JSON to validate.
     /// - Returns: `true` if the lexicon is valid, or `false` if not.
