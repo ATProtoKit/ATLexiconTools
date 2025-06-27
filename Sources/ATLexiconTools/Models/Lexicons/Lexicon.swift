06710cab3f8b05a9c75019c4e8ccf2218ce78410
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-06-26 22:25:46 -0400

    Add more error handling for the main lexicon structure
    - Check to see if the NSID of the lexicon is valid.
    - Check to see if records, procedures, queries, and subscriptions
    have the "main" definition.

--- Sources/ATLexiconTools/Models/Lexicons/Lexicon.swift
+++ Sources/ATLexiconTools/Models/Lexicons/Lexicon.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import ATSyntaxTools
 
 /// A structure representing the lexicon.
 public struct Lexicon: Codable {
@@ -26,6 +27,40 @@ public struct Lexicon: Codable {
 
     /// A dictionary of definitions within the lexicon.
     public let definitions: [String: LexiconDefinition]
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.lexicon = try container.decode(Int.self, forKey: .lexicon)
+        self.id = try container.decode(String.self, forKey: .id)
+
+        guard NSIDValidator.isValid(self.id) == true else {
+            throw DecodingError.dataCorruptedError(
+                forKey: .id,
+                in: container,
+                debugDescription: "'\(self.id)' is not a valid NSID."
+            )
+        }
+
+        self.revision = try container.decodeIfPresent(Int.self, forKey: .revision)
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.definitions = try container.decode([String: LexiconDefinition].self, forKey: .definitions)
+
+        for (key, definition) in self.definitions {
+            switch definition {
+                case .record, .query, .procedure, .subscription:
+                    if key != "main" {
+                        throw DecodingError.dataCorruptedError(
+                            forKey: .definitions,
+                            in: container,
+                            debugDescription: "Records, procedures, queries, and subscriptions must be the main definition (found '\(key)' instead)."
+                        )
+                    }
+                default:
+                    continue
+            }
+        }
+    }
 
     enum CodingKeys: String, CodingKey {
         case lexicon
