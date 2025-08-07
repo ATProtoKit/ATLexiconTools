6fe31c7dce3ac3229360ec68b33f576994e22e04
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-08-07 10:26:39 -0400

    Add BlobReferences

---
+++ Sources/ATLexiconTools/BlobReferences.swift
@@ -0,0 +1,234 @@
+//
+//  BlobReferences.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-06-26.
+//
+
+import Foundation
+import MultiformatsKit
+import ATCommonWeb
+
+/// A typed version of the blob reference JSON.
+///
+/// This version includes a size property, as well as a `CID` object.
+public struct TypedJSONBlobReference: Codable {
+
+    /// The reference type.
+    ///
+    /// It will always say "`blob`".
+    public let type: String = "blob"
+
+    /// The CID of the blob.
+    public let reference: CID
+
+    /// The mime type of the blob.
+    public let mimeType: String
+
+    /// The blob's size.
+    public let size: Int
+
+    enum CodingKeys: String, CodingKey {
+        case type = "$type"
+        case reference = "ref"
+        case mimeType
+        case size
+    }
+}
+
+/// An untyped version of the blob reference JSON.
+///
+/// This version lacks a size property, as well as a CID in its string representation.
+public struct UntypedJSONBlobReference: Codable {
+
+    /// The CID of the blob.
+    public let cid: String
+
+    /// The mime type of the blob.
+    public let mimeType: String
+}
+
+/// An enumeration that can represent either a typed or untyped JSON blob reference.
+public enum JSONBlobReference: Codable {
+
+    /// A typed blob reference.
+    case typed(TypedJSONBlobReference)
+
+    /// An untyped blob reference.
+    case untyped(UntypedJSONBlobReference)
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(TypedJSONBlobReference.self) {
+            self = .typed(value)
+        } else if let value = try? container.decode(UntypedJSONBlobReference.self) {
+            self = .untyped(value)
+        } else {
+            throw DecodingError.typeMismatch(
+                JSONBlobReference.self, DecodingError.Context(
+                    codingPath: decoder.codingPath, debugDescription: "Failed to decode the blob reference."))
+        }
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case typed
+        case untyped
+    }
+}
+
+/// A structure representing a blob reference.
+public struct BlobReference: Codable {
+
+    /// The reference type.
+    ///
+    /// It will always say "`blob`".
+    public let type: String = "blob"
+
+    /// The CID of the blob.
+    public let reference: CID
+
+    /// The mime type of the blob.
+    public let mimeType: String
+
+    /// The blob's size.
+    public let size: Int
+
+    /// Initializes an instance of `BlobReference`.
+    ///
+    /// - Parameters:
+    ///   - reference: The `CID` of the blob.
+    ///   - mimeType: The MIME type of the blob.
+    ///   - size: The size of the blob, in bytes.
+    public init(reference: CID, mimeType: String, size: Int) {
+        self.reference = reference
+        self.mimeType = mimeType
+        self.size = size
+    }
+
+    /// Converts a `JSONBlobReference` (either typed or untyped) into a `BlobReference`.
+    ///
+    /// - Parameter jsonReference: The `JSONBlobReference` value to convert.
+    /// - Returns: A new instance of `BlobReference`.
+    ///
+    /// - Throws: An error if `CID` conversion fails.
+    public static func convertToBlobReference(from jsonReference: JSONBlobReference) throws -> BlobReference {
+        switch jsonReference {
+            case .typed(let typedJSONBlobReference):
+                return BlobReference(
+                    reference: typedJSONBlobReference.reference,
+                    mimeType: typedJSONBlobReference.mimeType,
+                    size: typedJSONBlobReference.size
+                )
+            case .untyped(let untypedJSONBlobReference):
+                return BlobReference(
+                    reference: try CID(string: untypedJSONBlobReference.cid),
+                    mimeType: untypedJSONBlobReference.mimeType,
+                    size: -1
+                )
+        }
+    }
+
+    /// Converts this `BlobReference` object to an IPLD-compatible representation.
+    ///
+    /// - Returns: An `EncodableBlobReference` object that matches the IPLD blob schema.
+    ///
+    /// - Throws: An error if the `CID` cannot be converted to its canonical string.
+    public func toIPLDRepresentation() throws -> EncodableBlobReference {
+        let blobLink = try BlobLink(self.reference)
+        return EncodableBlobReference(
+            reference: blobLink,
+            mimeType: self.mimeType,
+            size: self.size
+        )
+    }
+
+    /// Serializes this blob reference to a formatted JSON string.
+    ///
+    /// - Returns: A JSON string representation of the blob reference.
+    ///
+    /// - Throws: An error if encoding fails.
+    public func toJSONRepresentation() throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+
+        let encodable = try self.toIPLDRepresentation()
+        let jsonData = try encoder.encode(encodable)
+
+        guard let jsonString = String(data: jsonData, encoding: .utf8) else {
+            throw EncodingError.invalidValue(
+                jsonData,
+                EncodingError.Context(
+                    codingPath: [],
+                    debugDescription: "Could not convert IPLD representation of BlobReference to JSON."
+                )
+            )
+        }
+
+        return jsonString
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case type = "$type"
+        case reference = "ref"
+        case mimeType
+        case size
+    }
+}
+
+/// A structure that models the IPLD-compatible representation of a blob reference.
+public struct EncodableBlobReference: Codable {
+
+    /// The reference type.
+    ///
+    /// This will always have the value "`blob`".
+    public let type: String = "blob"
+
+    /// The wrapped content identifier, encoded as a `$link` property.
+    public let reference: BlobLink
+
+    /// The MIME type of the blob.
+    public let mimeType: String
+
+    /// The size of the blob, in bytes.
+    public let size: Int
+
+    /// Initializes a new `EncodableBlobReference` with the specified values.
+    ///
+    /// - Parameters:
+    ///   - reference: The blob's link (as a `BlobLink` struct).
+    ///   - mimeType: The MIME type of the blob.
+    ///   - size: The size of the blob, in bytes.
+    public init(reference: BlobLink, mimeType: String, size: Int) {
+        self.reference = reference
+        self.mimeType = mimeType
+        self.size = size
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case type = "$type"
+        case reference = "ref"
+        case mimeType
+        case size
+    }
+}
+
+/// A container that wraps the `$link` property inside of the blob structure.
+public struct BlobLink: Codable {
+
+    /// The `$link` property itself.
+    public let link: String
+
+    /// Initializes an instance of `BlobLink`.
+    ///
+    /// - Parameter cid: The `CID` object to extract.
+    ///
+    /// - Throws: An error if CID conversion to its string representation fails.
+    public init(_ cid: CID) throws {
+        self.link = try cid.canonicalString
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case link = "$link"
+    }
+}
