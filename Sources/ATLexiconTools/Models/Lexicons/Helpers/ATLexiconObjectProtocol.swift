9d04218d8412062a3c578967f0e53e7531692f96
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-08-09 04:56:54 -0400

    Add ATLexiconObjectProtocol to use for type checking

---
+++ Sources/ATLexiconTools/Models/Lexicons/Helpers/ATLexiconObjectProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ATLexiconObjectProtocol.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-08-09.
+//
+
+/// A protocol representing a lexicon object.
+public protocol ATLexiconObjectProtocol: Codable {
+
+    /// The type value of the lexicon object.
+    var type: String { get }
+}
