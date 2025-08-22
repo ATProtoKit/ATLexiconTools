bb81a7096083d6391245fc1779bf08bdfc3923ae
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-08-22 11:17:06 -0400

    Add public initializer and Codable methods to ATBlobType

--- Sources/ATLexiconTools/Models/Lexicons/Lexicon Types/ATBlobType.swift
+++ Sources/ATLexiconTools/Models/Lexicons/Lexicon Types/ATBlobType.swift
@@ -26,6 +26,34 @@ public struct ATBlobType: ATLexiconObjectProtocol {
     /// The maximum size of the blob in bytes. Optional.
     public let maximumSize: Int?
 
+    /// Creates an instance of `ATBlobType`.
+    ///
+    /// - Parameters:
+    ///   - description: A short description of the object. Optional.
+    ///   - accept: An array of MIME types in a glob pattern (example: `image/jpeg`, `*/*`, etc.). Optional.
+    ///   - maximumSize: The maximum size of the blob in bytes. Optional.
+    public init(description: String?, accept: [String]?, maximumSize: Int?) {
+        self.description = description
+        self.accept = accept
+        self.maximumSize = maximumSize
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.accept = try container.decodeIfPresent([String].self, forKey: .accept)
+        self.maximumSize = try container.decodeIfPresent(Int.self, forKey: .maximumSize)
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encodeIfPresent(self.description, forKey: .description)
+        try container.encodeIfPresent(self.accept, forKey: .accept)
+        try container.encodeIfPresent(self.maximumSize, forKey: .maximumSize)
+    }
+
     enum CodingKeys: String, CodingKey {
         case description
         case accept
