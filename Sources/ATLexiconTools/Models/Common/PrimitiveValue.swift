4fb5f70c2ebaf14de85ac1d2c45967bb0906f091
Christopher Jr Riley <chrisjr@cjrriley.com>
2026-02-26 05:13:01 -0500

    Add PrimitiveValue

---
+++ Sources/ATLexiconTools/Models/Common/PrimitiveValue.swift
@@ -0,0 +1,203 @@
+//
+//  PrimitiveValue.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2026-02-22.
+//
+
+import Foundation
+import MultiformatsKit
+
+/// An enumeration representing a primitive value.
+public enum PrimitiveValue: Codable, Sendable {
+
+    /// A `nil` value.
+    case `nil`
+
+    /// A `Bool` value.
+    case bool(Bool)
+
+    /// An `Int` value.
+    case int(Int)
+
+    /// A `Double` value.
+    case double(Double)
+
+    /// A `String` value.
+    case string(String)
+
+    /// A `Data` value.
+    case bytes(Data)
+
+    /// A `CID` value.
+    case cid(CID)
+
+    /// A blob reference value.
+    case blob(BlobReference)
+
+    /// An array value.
+    case array([PrimitiveValue])
+
+    /// A dictionary value.
+    case dictionary([String: PrimitiveValue])
+
+    /// Returns the wrapped object value.
+    public var dictionaryValue: [String: PrimitiveValue]? {
+        guard case .dictionary(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Returns the wrapped array value.
+    public var arrayValue: [PrimitiveValue]? {
+        guard case .array(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Returns the wrapped string value.
+    public var stringValue: String? {
+        guard case .string(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Returns the wrapped integer value.
+    public var integerValue: Int? {
+        guard case .int(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Returns the wrapped boolean value.
+    public var booleanValue: Bool? {
+        guard case .bool(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Returns the wrapped binary value.
+    public var bytesValue: Data? {
+        guard case .bytes(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Returns the wrapped CID value.
+    public var cidValue: CID? {
+        guard case .cid(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Returns the wrapped blob reference value.
+    public var blobValue: BlobReference? {
+        guard case .blob(let value) = self else {
+            return nil
+        }
+        return value
+    }
+
+    /// Indicates whether the value is null.
+    public var isNil: Bool {
+        if case .nil = self {
+            return true
+        }
+        return false
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        if let value = try? container.decode(Bool.self) {
+            self = .bool(value)
+        } else if let value = try? container.decode(Int.self) {
+            self = .int(value)
+        } else if let value = try? container.decode(Double.self) {
+            self = .double(value)
+        } else if let value = try? container.decode(String.self) {
+            self = .string(value)
+        } else if let value = try? container.decode([PrimitiveValue].self) {
+            self = .array(value)
+        } else if let value = try? container.decode([String: PrimitiveValue].self) {
+            self = .dictionary(value)
+        } else {
+            throw DecodingError.typeMismatch(
+                CodableValue.self,
+                DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Unsupported value"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        switch self {
+            case .nil:
+                try container.encodeNil()
+            case .bool(let value):
+                try container.encode(value)
+            case .int(let value):
+                try container.encode(value)
+            case .double(let value):
+                try container.encode(value)
+            case .string(let value):
+                try container.encode(value)
+            case .bytes(let value):
+                try container.encode(value)
+            case .cid(let value):
+                try container.encode(value)
+            case .blob(let value):
+                try container.encode(value)
+            case .array(let value):
+                try container.encode(value)
+            case .dictionary(let value):
+                try container.encode(value)
+        }
+    }
+}
+
+extension PrimitiveValue: ExpressibleByNilLiteral, ExpressibleByBooleanLiteral, ExpressibleByIntegerLiteral, ExpressibleByFloatLiteral,
+                               ExpressibleByStringLiteral, ExpressibleByArrayLiteral, ExpressibleByDictionaryLiteral {
+
+    public init(nilLiteral: ()) {
+        self = .nil
+    }
+
+    public init(booleanLiteral value: Bool) {
+        self = .bool(value)
+    }
+
+
+    public init(integerLiteral value: Int) {
+        self = .int(value)
+    }
+
+    public init(floatLiteral value: Double) {
+        self = .double(value)
+    }
+
+    public init(stringLiteral value: String) {
+        self = .string(value)
+    }
+
+    public init(arrayLiteral elements: PrimitiveValue...) {
+        self = .array(elements)
+    }
+
+    public init(dictionaryLiteral elements: (String, PrimitiveValue)...) {
+        var dictionary: [String: PrimitiveValue] = [:]
+        for (key, value) in elements {
+            dictionary[key] = value
+        }
+        self = .dictionary(dictionary)
+    }
+}
--- Sources/ATLexiconTools/Models/Lexicons/Lexicon Types/ATStringType.swift
+++ Sources/ATLexiconTools/Models/Lexicons/Lexicon Types/ATStringType.swift
@@ -163,4 +163,31 @@ public struct ATStringType: ATLexiconObjectProtocol {
         /// Indicates the format is a language code.
         case language
     }
+    /// Validates the string value based on their format.
+    private static func validateFormat(format: Format, value: String) throws {
+        switch format {
+            case .atIdentifier:
+                try Validator.Format.validateATIdentifier(path: "", atIdentifier: value)
+            case .atURI:
+                try Validator.Format.validateATURI(path: "", atURIValue: value)
+            case .cid:
+                try Validator.Format.validateCID(path: "", cidValue: value)
+            case .dateTime:
+                try Validator.Format.validateDateTime(path: "", dateTimeValue: value)
+            case .did:
+                try Validator.Format.validateDID(path: "", didValue: value)
+            case .handle:
+                try Validator.Format.validateHandle(path: "", handleValue: value)
+            case .nsid:
+                try Validator.Format.validateNSID(path: "", nsidValue: value)
+            case .tid:
+                try Validator.Format.validateTID(path: "", tidValue: value)
+            case .recordKey:
+                try Validator.Format.validateRecordKey(path: "", recordKeyValue: value)
+            case .uri:
+                try Validator.Format.validateURI(path: "", uriValue: value)
+            case .language:
+                try Validator.Format.validateLanguage(path: "", languageValue: value)
+        }
+    }
 }
