d3514c6ffaefbd04442443fd06063bf537d81b4a
Christopher Jr Riley <chrisjr@cjrriley.com>
2026-02-26 04:53:52 -0500

    Move Validator file to Validators folder

---
+++ Sources/ATLexiconTools/Validators/Validator.swift
@@ -0,0 +1,25 @@
+//
+//  Validator.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2025-08-07.
+//
+
+/// Utility functions to validate lexicon JSON inputs and outputs.
+public enum Validator {
+
+    /// Functions for validating blob-related JSON inputs and outputs.
+    public enum Blob {}
+
+    /// Functions for validating non-primitive-related JSON inputs and outputs.
+    public enum Complex {}
+
+    /// Functions for validating format-related JSON inputs and outputs.
+    public enum Format {}
+
+    /// Functions for validating primitive-related JSON inputs and outputs.
+    public enum Primitive {}
+
+    /// Functions for validating schema-related JSON inputs and outputs.
+    public enum Schema {}
+}
