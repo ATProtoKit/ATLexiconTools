404a5f737ec3acf3c6b95f4d3ed83b80fee6a342
Christopher Jr Riley <chrisjr@cjrriley.com>
2026-03-16 02:12:37 -0400

    Add validators and throwing initializer

--- Sources/ATLexiconTools/Models/Lexicons/Primary Definitions/Lexicon.swift
+++ Sources/ATLexiconTools/Models/Lexicons/Primary Definitions/Lexicon.swift
@@ -41,12 +41,17 @@ public struct Lexicon: Codable, Sendable {
     ///   - revision: The revision number of the lexicon to denote changes. Optional. Defaults to `nil`.
     ///   - description: A short description of the lexicon. Optional. Defaults to `nil`.
     ///   - definitions: A dictionary of definitions within the lexicon.
-    public init(lexicon: Int, id: String, revision: Int? = nil, description: String? = nil, definitions: [String : LexiconDefinition]) {
+    ///
+    ///   - Throws: An error if a value is violating the lexicon requirements.
+    public init(lexicon: Int, id: String, revision: Int? = nil, description: String? = nil, definitions: [String : LexiconDefinition]) throws {
         self.lexicon = lexicon
         self.id = id
         self.revision = revision
         self.description = description
         self.definitions = definitions
+
+        try validate()
+        try Self.validate(id: self.id)
     }
 
     public init(from decoder: Decoder) throws {
@@ -55,32 +60,13 @@ public struct Lexicon: Codable, Sendable {
         self.lexicon = try container.decode(Int.self, forKey: .lexicon)
         self.id = try container.decode(String.self, forKey: .id)
 
-        guard NSIDValidator.isValid(self.id) else {
-            throw DecodingError.dataCorruptedError(
-                forKey: .id,
-                in: container,
-                debugDescription: "The NSID provided is invalid."
-            )
-        }
+        try Self.validate(id: self.id, container: container)
 
         self.revision = try container.decodeIfPresent(Int.self, forKey: .revision)
         self.description = try container.decodeIfPresent(String.self, forKey: .description)
         self.definitions = try container.decode([String: LexiconDefinition].self, forKey: .definitions)
 
-        for (key, definition) in definitions {
-            switch definition {
-                case .record, .query, .procedure, .subscription:
-                    if key != "main" {
-                        throw DecodingError.dataCorruptedError(
-                            forKey: .definitions,
-                            in: container,
-                            debugDescription: "Records, procedures, queries, and subscriptions must be in the 'main' definition (found '\(key)' instead)."
-                        )
-                    }
-                default:
-                    continue
-            }
-        }
+        try validate(container)
     }
 
     enum CodingKeys: String, CodingKey {
@@ -89,5 +75,56 @@ public struct Lexicon: Codable, Sendable {
         case revision
         case description
         case definitions = "defs"
+    }
+
+    // MARK: - Validators
+
+    /// Validates the current level of the lexicon.
+    ///
+    /// - Parameter container: A keyed decoding container view into this decoder. Optional. Defaults to `nil`.
+    ///
+    /// - Throws: An error if the encountered stored value is not in the "main" definition.
+    private func validate(_ container: KeyedDecodingContainer<Lexicon.CodingKeys>? = nil) throws {
+        for (key, definition) in definitions {
+            switch definition {
+                case .record, .query, .procedure, .subscription:
+                    if key != "main" {
+                        if let container = container {
+                            throw DecodingError.dataCorruptedError(
+                                forKey: .definitions,
+                                in: container,
+                                debugDescription: "Records, procedures, queries, and subscriptions must be in the 'main' definition (found '\(key)' instead)."
+                            )
+                        } else {
+                            throw LexiconSchemaValidatorError.invalidSchema(
+                                reason: "Records, procedures, queries, and subscriptions must be in the 'main' definition (found '\(key)' instead).")
+                        }
+
+                    }
+                default:
+                    continue
+            }
+        }
+    }
+
+    /// Validates the Namespaced Identifier (NSID) of the lexicon.
+    ///
+    /// - Parameters:
+    ///   - id: The NSID of the lexicon.
+    ///   - container: A keyed decoding container view into this decoder.
+    ///
+    /// - Throws: An error if the encountered stored value is invalid.
+    private static func validate(id: String, container: KeyedDecodingContainer<Lexicon.CodingKeys>? = nil) throws {
+        guard NSIDValidator.isValid(id) else {
+            if let container = container {
+                throw DecodingError.dataCorruptedError(
+                    forKey: .id,
+                    in: container,
+                    debugDescription: "The NSID provided is invalid."
+                )
+            } else {
+                throw LexiconSchemaValidatorError.invalidSchema(reason: "The NSID provided is invalid.")
+            }
+        }
     }
 }
