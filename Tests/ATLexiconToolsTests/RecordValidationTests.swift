441476f7cd1b43cfece87e40f93d17311927ed20
Christopher Jr Riley <chrisjr@cjrriley.com>
2026-03-18 23:08:47 -0400

    Add unit test that passes valid schemas

---
+++ Tests/ATLexiconToolsTests/RecordValidationTests.swift
@@ -0,0 +1,56 @@
+//
+//  RecordValidationTests.swift
+//  ATLexiconTools
+//
+//  Created by Christopher Jr Riley on 2026-03-18.
+//
+
+import Foundation
+import Testing
+import MultiformatsKit
+@testable import ATLexiconTools
+
+@Suite
+struct `Record Validation` {
+
+    public var lexiconRegistry: LexiconRegistry
+
+    public init() throws {
+        self.lexiconRegistry = try makeLexicons()
+    }
+
+    private func makePassingSink() throws -> PrimitiveValue {
+        return .object([
+            "$type": "com.example.kitchenSink",
+            "object": [
+                "object": ["boolean": true],
+                "array": ["one", "two"],
+                "boolean": true,
+                "integer": 123,
+                "string": "string"
+            ],
+            "array": ["one", "two"],
+            "boolean": true,
+            "integer": 123,
+            "string": "string",
+            "bytes": .bytes(Data([0, 1, 2, 3])),
+            "cidLink": .cid(try CID(string: "bafyreidfayvfuwqa7qlnopdjiqrxzs6blmoeu4rujcjtnci5beludirz2a"))
+        ])
+    }
+
+    @Test
+    func `Passes valid schemas`() throws {
+        #expect(throws: Never.self) {
+            try lexiconRegistry.validateRecord(
+                by: "com.example.kitchenSink",
+                value: makePassingSink()
+            )
+        }
+    }
+
+//    @Test
+//    func ``() async throws {
+//        <#body#>
+//    }
+
+}
