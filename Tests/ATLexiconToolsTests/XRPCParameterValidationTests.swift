79aec26e1c9094547f929abaee19021640c94706
Christopher Jr Riley <chrisjr@cjrriley.com>
2026-03-28 09:38:20 -0400

    Add unit test for passing valid parameters

--- Tests/ATLexiconToolsTests/XRPCParameterValidationTests.swift
+++ Tests/ATLexiconToolsTests/XRPCParameterValidationTests.swift
@@ -29,4 +29,42 @@ struct `XRPC Parameter Validation` {
             "string": "string"
         ])
     }
+
+    @Test
+    func `Passes valid parameters`() throws {
+        let queryResult = try lexiconRegistry.validateXRPCParameters(
+            by: "com.example.query",
+            value: .object([
+                "boolean": true,
+                "integer": 123,
+                "string": "string",
+                "array": ["x", "y"]
+            ])
+        )
+        #expect(queryResult == .object([
+            "boolean": true,
+            "integer": 123,
+            "string": "string",
+            "array": ["x", "y"],
+            "def": 0
+        ]))
+
+        let procedureResult = try lexiconRegistry.validateXRPCParameters(
+            by: "com.example.procedure",
+            value: .object([
+                "boolean": true,
+                "integer": 123,
+                "string": "string",
+                "array": ["x", "y"],
+                "def": 1
+            ])
+        )
+        #expect(procedureResult == .object([
+            "boolean": true,
+            "integer": 123,
+            "string": "string",
+            "array": ["x", "y"],
+            "def": 1
+        ]))
+    }
 }
